430b71aab606fe19882ddb09bd04ba9ae38dffdd
Rima971 <madhurima.poddar_ftc@external.swiggy.in>
2024-03-11 08:38:34 +0530

    added security configuratioins without authorization

---
+++ src/main/java/com/swiggy/authenticator/configurations/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.swiggy.authenticator.configurations;
+
+import com.swiggy.authenticator.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfiguration {
+    @Autowired
+    UsersService usersService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(this.usersService).passwordEncoder(this.passwordEncoder());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests((authorize) -> {
+                    authorize
+                            .requestMatchers(HttpMethod.POST,"/api/users/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/api/customers/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/api/delivery-agents/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/api/admins/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .authenticationManager(authenticationManagerBuilder.build())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+}
